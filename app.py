50896cb9fee59ef0f165d234fe7477c707af6df4
KarimovProject <abdullatifkarimov772@gmail.com>
2023-02-10 18:26:23 +0500

    fixed problem

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
+# @app.route('/')
+# def hello_world():
+#     return "Hello world"
+
 @app.route('/')
-def hello_world():
-    return "Hello world"
+def hom():
+    r = request.args
+    a = int(r['a'])
+    b = int(r['b'])
+    s = a+b
+    
+    return f'summa: {s}'
 
 if __name__=='__main__':
     app.run()
